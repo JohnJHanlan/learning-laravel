d0a47172312c64aef3e6c8bec84223bf7cf04d1d
John Hanlan <JohnJHanlan@gmail.com>
2018-10-02 16:17:19 -0500

    Created partial and included header in master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,15 @@
 
         <title>Laravel</title>
 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> --}}
+
     </head>
     <body>
-        @yield('content')
+        @include('partials.header')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
