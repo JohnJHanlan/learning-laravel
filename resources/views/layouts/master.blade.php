11d325aba2bf93f2eb4dd1a6952b2c572f4d9f37
John Hanlan <JohnJHanlan@gmail.com>
2018-10-09 21:51:50 -0500

    Routes and Views

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,22 +1,21 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    <title>Laravel</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> --}}
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Arizonia" type="text/css">
-        <link rel="stylesheet" href=" {{ URL::to('css/styles.css') }}">
-    </head>
-    <body>
-        @include('partials.header')
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+    <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
+</head>
+<body>
+@include('partials.header')
+<div class="container">
+    @yield('content')
+</div>
+</body>
 </html>
